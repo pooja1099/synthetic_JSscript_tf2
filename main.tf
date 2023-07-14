44bf47bf8f34408638b5409a1e608f9aaac57a6f
pooja1099 <poojasangwan0510@gmail.com>
2023-07-14 11:37:44 +0530

    done

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "name" {
-    source = "git::https://github.com/pooja1099/synthetic_JSscript_tf.git"
+    source = "git::https://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@github.com/pooja1099/synthetic_JSscript_tf.git"
     data1 = var.data1
     data2 = var.data2
   
